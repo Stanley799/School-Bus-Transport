475b413695c28a65d1f1cdd4b6b2be23b9191f92
Stanley799 <oduor.otieno@strathmore.edu>
2025-06-11 22:27:55 +0300

    cb connection with node.js using docker and dbeaver

---
+++ school-bus-system/source-code/server.js
@@ -0,0 +1,20 @@
+//testing the dbpostgres and node.js connection
+const express = require('express');
+const pool = require('./db_node'); //the connection file 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Database time: ${result.rows[0].now}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
